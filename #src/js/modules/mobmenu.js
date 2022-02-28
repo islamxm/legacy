d12a37b21239d157c0c1842cb05eb3ef83253f4a
Islam <isxmad@gmail.com>
2022-02-28 08:25:14 +0300

    mobm fix

--- #src/js/modules/mobmenu.js
+++ #src/js/modules/mobmenu.js
@@ -7,7 +7,18 @@ export const mobmenu = () => {
 
 
     const navItems = document.querySelectorAll('[data-nav]');
+    const navLi = document.querySelectorAll('.mobmenu__nav_item');
 
+    if(navLi.length > 0) {
+        navLi.forEach(item => {
+            item.addEventListener('click', () => {
+                navLi.forEach(i => {
+                    i.classList.remove('active');
+                })
+                item.classList.add('active');
+            })
+        })
+    }
     if(navItems.length > 0) {
         navItems.forEach(item => {
             item.addEventListener('click', () => {
